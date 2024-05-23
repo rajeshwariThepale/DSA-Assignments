c0d078943c511d9cc85e8271039dae8dfbdf6e3a
rajeshwariT20 <rajeshwari.thepale@gmail.com>
2024-05-23 22:25:44 +0530

    one file added

---
+++ assignment03/missingNumber.js
@@ -0,0 +1,20 @@
+//program to find missing numbers from array
+
+function missingNumber(array) {
+
+    const n = array.length +1;
+    const total = (n * (n + 1)) / 2;
+
+    let sumOfArray = 0;
+    for (let i = 0; i < n-1; i++) {
+       sumOfArray = sumOfArray + array[i];
+    }
+    let missingNum = total - sumOfArray;
+    return missingNum;
+};
+
+// to print the missing number
+const array=[1,2,4,5,6,7];
+const result = missingNumber(array);
+
+console.log(result);
